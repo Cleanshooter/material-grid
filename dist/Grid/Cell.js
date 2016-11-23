49534b9b88de2cd7b8367e470ddc547675e3ddde
Joe Motacek <cleanshooter@gmail.com>
2016-11-23 15:07:54 -0600

    Added more to the cells...
    Signed-off-by: Joe Motacek <cleanshooter@gmail.com>

--- dist/Grid/Cell.js
+++ dist/Grid/Cell.js
@@ -17,7 +17,48 @@ var GenerateCellClassNames = function GenerateCellClassNames(props) {
   if (props.col) {
     classes += " mdl-cell--" + props.col + "-col";
   }
-  console.log(props);
+  if (props.desktop) {
+    classes += " mdl-cell--" + props.desktop + "-col-desktop";
+  }
+  if (props.tablet) {
+    classes += " mdl-cell--" + props.tablet + "-col-tablet";
+  }
+  if (props.phone) {
+    classes += " mdl-cell--" + props.phone + "-col-phone";
+  }
+  if (props.offset) {
+    classes += " mdl-cell--" + props.offset + "-offset";
+  }
+  if (props.offsetDesktop) {
+    classes += " mdl-cell--" + props.offsetDesktop + "-offset-desktop";
+  }
+  if (props.offsetTablet) {
+    classes += " mdl-cell--" + props.offsetTablet + "-offset-tablet";
+  }
+  if (props.offsetPhone) {
+    classes += " mdl-cell--" + props.offsetPhone + "-offset-phone";
+  }
+  if (props.hideDesktop) {
+    classes += " mdl-cell--hide-desktop";
+  }
+  if (props.hideTablet) {
+    classes += " mdl-cell--hide-tablet";
+  }
+  if (props.hidePhone) {
+    classes += " mdl-cell--hide-phone";
+  }
+  if (props.stretch) {
+    classes += " mdl-cell--stretch";
+  }
+  if (props.top) {
+    classes += " mdl-cell--top";
+  }
+  if (props.middle) {
+    classes += " mdl-cell--middle";
+  }
+  if (props.bottom) {
+    classes += " mdl-cell--bottom";
+  }
   return classes;
 };
 
--- src/Grid/Cell.js
+++ src/Grid/Cell.js
@@ -4,9 +4,50 @@ import React from 'react';
 const GenerateCellClassNames = (props) => {
   let classes = "mdl-cell";
   if(props.col){
-    classes += " mdl-cell--"+props.col+"-col"
+    classes += " mdl-cell--"+props.col+"-col";
   }
-  console.log(props);
+  if(props.desktop){
+    classes += " mdl-cell--"+props.desktop+"-col-desktop";
+  }
+  if(props.tablet){
+    classes += " mdl-cell--"+props.tablet+"-col-tablet";
+  }
+  if(props.phone){
+    classes += " mdl-cell--"+props.phone+"-col-phone";
+  }
+  if(props.offset){
+    classes += " mdl-cell--"+props.offset+"-offset";
+  }
+  if(props.offsetDesktop){
+    classes += " mdl-cell--"+props.offsetDesktop+"-offset-desktop";
+  }
+  if(props.offsetTablet){
+    classes += " mdl-cell--"+props.offsetTablet+"-offset-tablet";
+  }
+  if(props.offsetPhone){
+    classes += " mdl-cell--"+props.offsetPhone+"-offset-phone";
+  }
+  if(props.hideDesktop){
+    classes += " mdl-cell--hide-desktop";
+  }
+  if(props.hideTablet){
+    classes += " mdl-cell--hide-tablet";
+  }
+  if(props.hidePhone){
+    classes += " mdl-cell--hide-phone";
+  }
+  if(props.stretch){
+    classes += " mdl-cell--stretch";
+  }
+  if(props.top){
+    classes += " mdl-cell--top";
+  }
+  if(props.middle){
+    classes += " mdl-cell--middle";
+  }
+  if(props.bottom){
+    classes += " mdl-cell--bottom";
+  }
   return classes;
 }
 
